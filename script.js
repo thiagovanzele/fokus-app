9d814841a5b532cfe0ebf837daeffe27e214e5a3
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-06 18:11:43 -0300

    feat: alterada a foto a partir do evento de click

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ const html = document.querySelector('html');
 const botaoFoco = document.querySelector('.app__card-button--foco');
 const botaoCurto = document.querySelector('.app__card-button--curto');
 const botaoLongo = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
+
 
 botaoFoco.addEventListener('click', (e) => {
     html.setAttribute('data-contexto', 'foco');
+    banner.setAttribute('src', '/imagens/foco.png')
 });
 
 botaoCurto.addEventListener('click', (e) => {
     html.setAttribute('data-contexto', 'descanso-curto');
+    banner.setAttribute('src', '/imagens/descanso-curto.png');
 });
 
 botaoLongo.addEventListener('click', (e) => {
     html.setAttribute('data-contexto', 'descanso-longo');
+    banner.setAttribute('src', '/imagens/descanso-longo.png');
 });
