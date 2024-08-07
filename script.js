2a5a54d5de99f93d2c2c1710acdbcd628b4a5506
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-07 19:04:26 -0300

    feat: adicionado funcao para selecionar botao conforme contexto

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `imagens/${contexto}.png`);
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `Hora de voltar à superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+        default:
+            break;
+
+    }
+}
+
+function alterarAtivo(elementosAtivos, elementoParaAtivar) {
+    elementosAtivos.forEach(el => el.classList.remove('active'));
+    elementoParaAtivar.classList.add('active');
+}
+
 const html = document.querySelector('html');
-const botaoFoco = document.querySelector('.app__card-button--foco');
-const botaoCurto = document.querySelector('.app__card-button--curto');
-const botaoLongo = document.querySelector('.app__card-button--longo');
+const focoBt = document.querySelector('.app__card-button--foco');
+const curtoBt = document.querySelector('.app__card-button--curto');
+const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 
 
-botaoFoco.addEventListener('click', (e) => {
-    html.setAttribute('data-contexto', 'foco');
-    banner.setAttribute('src', '/imagens/foco.png')
+focoBt.addEventListener('click', (e) => {
+    alterarContexto('foco');
+    alterarAtivo([curtoBt, longoBt], focoBt)
 });
 
-botaoCurto.addEventListener('click', (e) => {
-    html.setAttribute('data-contexto', 'descanso-curto');
-    banner.setAttribute('src', '/imagens/descanso-curto.png');
+curtoBt.addEventListener('click', (e) => {
+    alterarContexto('descanso-curto');
+    alterarAtivo([focoBt, longoBt], curtoBt)
 });
 
-botaoLongo.addEventListener('click', (e) => {
-    html.setAttribute('data-contexto', 'descanso-longo');
-    banner.setAttribute('src', '/imagens/descanso-longo.png');
+longoBt.addEventListener('click', (e) => {
+    alterarContexto('descanso-longo');
+    alterarAtivo([focoBt, curtoBt], longoBt)
 });
